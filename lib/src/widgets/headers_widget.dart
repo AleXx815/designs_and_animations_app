5148b0c86e7be3b549bb10ed4e116c42cb4c3f1c
AleXx815 <davgoga815@gmail.com>
2023-09-05 18:38:19 -0500

    CircularBorderHeader widget

--- lib/src/widgets/headers_widget.dart
+++ lib/src/widgets/headers_widget.dart
@@ -11,3 +11,21 @@ class SquareHeader extends StatelessWidget {
     );
   }
 }
+
+class CircularBorderHeader extends StatelessWidget {
+  const CircularBorderHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      decoration: const BoxDecoration(
+        color: Colors.pink,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(70),
+          bottomRight: Radius.circular(70),
+        ),
+      ),
+    );
+  }
+}
